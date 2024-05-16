d686ad05f1c97ae30ae544ecd113472b10f9f924
pretzelogical <yingyangyo266@gmail.com>
2024-05-16 14:12:27 -0500

    Basic webpack react app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+export default function App() {
+  return (
+    <>
+      <header>
+        <h1>Teknet</h1>
+      </header>
+      <main>
+        <div className="postsDisplay">
+          <p>Post content goes here</p>
+        </div>
+      </main>
+      <footer>
+        <p>Teknet - 2024</p>
+      </footer>
+    </>
+  );
+}
